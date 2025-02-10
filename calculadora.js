2e1f1545d3e929aa02cefc743b963a53cdcb659e
emanuelsilvapereira <167905863+emanuelsilvapereira@users.noreply.github.com>
2025-02-10 17:01:48 -0300

    calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,4 @@
-// const prompt = require('prompt-sync')();
-
-const promptSync = require('prompt-sync');
-const prompt = promptSync();
+const prompt = require('prompt-sync')();
 
 function soma(a, b) {
     return a + b;
@@ -23,92 +20,65 @@ function divisao(a, b) {
         return "Erro: Divisão por zero";
     }
 }
-function raizquadrada(a) {
-    return sqrt(a);
 
-}
-function potenciação(a, b) {
-    return a**b
-    ;
-
+function exponenciação(a, b) {
+    return a ** b;
 }
 
-function Seno(a)  {
+function raiz(a){
+    return Math.sqrt (a)
+}
 
+function seno(a){
     return Math.sin(a)
-    
 }
 
-function cosseno(a)  {
+function cosseno(a){
     return Math.cos(a)
-    
 }
 
-function tangente(a)  {
+function tangente(a){
     return Math.tan(a)
-   
 }
 
 while (true) {
-    let escolha = prompt("Escolha a operação: 1. Soma, 2. Subtração, 3. Multiplicação, 4. divisão, 5. potenciação, 6. raiz quadrada, 7. seno, 8. cosseno, 9. tangente, 10. sair");
+    let escolha = prompt("Escolha a operação: 1. Soma, 2. Subtração, 3. Multiplicação, 4. divisão, 5. exponenciação, 6. raiz quadrada, 7. seno, 8. cosseno, 9. tangente, 10. sair: ");
 
     if (escolha === "10") {
         break;
     }
 
+    if (escolha === "6"|| escolha === "7"|| escolha === "8"|| escolha === "9"){
+        let num =  parseFloat(prompt("Digite o número: "));
+        if (escolha === "6") {
+            console.log("Resultado: ", raiz(num));
+        }else if (escolha === "7") {
+            console.log("Resultado: ", seno(num));
+        }else if (escolha === "8") {
+            console.log("Resultado: ", cosseno(num));
+        }else if (escolha === "9") {
+            console.log("Resultado: ", tangente(num));
+        }
+    }else{
+
+    let num1 = parseFloat(prompt("Digite o primeiro número: "));
+    let num2 = parseFloat(prompt("Digite o segundo número: "));
+
     if (escolha === "1") {
-        let num1 = parseFloat(prompt("Digite o primeiro número: "));
-        let num2 = parseFloat(prompt("Digite o segundo número: "));
         console.log("Resultado: ", soma(num1, num2));
-    } 
-    else if (escolha === "2") {
-        let num1 = parseFloat(prompt("Digite o primeiro número: "));
-        let num2 = parseFloat(prompt("Digite o segundo número: "));
+    } else if (escolha === "2") {
         console.log("Resultado: ", subtracao(num1, num2));
-    } 
-    
-    else if (escolha === "3") {
-        let num1 = parseFloat(prompt("Digite o primeiro número: "));
-        let num2 = parseFloat(prompt("Digite o segundo número: "));
+    } else if (escolha === "3") {
         console.log("Resultado: ", multiplicacao(num1, num2));
-    } 
-    
-    else if (escolha === "4") {
-        let num1 = parseFloat(prompt("Digite o primeiro número: "));
-        let num2 = parseFloat(prompt("Digite o segundo número: "));
+    } else if (escolha === "4") {
         console.log("Resultado: ", divisao(num1, num2));
-    } 
-    
-    else if (escolha === "5") {
-        let num1 = parseFloat(prompt("Digite o primeiro número: "));
-        let num2 = parseFloat(prompt("Digite o segundo número: "));
-        console.log("Resultado: ", potenciação(num1));
-    } 
-    
-    else if (escolha === "6") {
-        let num1 = parseFloat(prompt("Digite o número: "))
-        console.log("Resultado: ", raizquadrada(num1));
-    } 
-    
-    else if (escolha === "7") {
-        let num1 = parseFloat(prompt("Digite o valor do cateto oposto: "));
-        let num2 = parseFloat(prompt("Digite o valor da hipotenusa: "));
-        console.log("Resultado: ", Seno(num1, num2));
+    } else if (escolha === "5") {
+        console.log("Resultado: ", exponenciação(num1, num2));
+    } else if (escolha === "6") {
+        console.log("Resultado: ", raiz(num1));
     }
-
-    else if (escolha === "8") {
-        let num1 = parseFloat(prompt("Digite o valor do cateto adjacente: "));
-        let num2 = parseFloat(prompt("Digite o valor da hipotenusa: "));
-        console.log("Resultado: ", cosseno(num1, num2));
-    }
-
-    else if (escolha === "9") {
-        let num1 = parseFloat(prompt("Digite o valor do cateto oposto: "));
-        let num2 = parseFloat(prompt("Digite o valor do cateto adjacente: "));
-        console.log("Resultado: ", tangente(num1, num2));
-    } 
-    
     else {
         console.log("Escolha inválida");
+    }
     }
 }
